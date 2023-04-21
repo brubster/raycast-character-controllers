07a046a2d2b53138c4ec0f1fe07595186c7d3eb0
Evan <evan.c.ohme@gmail.com>
2023-04-20 22:53:43 -0700

    Removed extra process() call in pause menu script

--- src/CharController3D/scripts/gui/pause_menu.gd
+++ src/CharController3D/scripts/gui/pause_menu.gd
@@ -1,11 +1,6 @@
 extends Control
 
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and is_visible():
-		get_viewport().set_input_as_handled()
-
-
 func _on_resume_pressed() -> void:
 	hide()
 
