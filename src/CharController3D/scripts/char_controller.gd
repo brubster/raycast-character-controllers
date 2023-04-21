f9d1697b9d6e6f5b0020b1e596a6c64b072c185f
Evan <evan.c.ohme@gmail.com>
2023-04-21 00:12:33 -0700

    Add simple jumping, needs work

--- src/CharController3D/scripts/char_controller.gd
+++ src/CharController3D/scripts/char_controller.gd
@@ -19,7 +19,8 @@ extends CharacterBody3D
 @onready var camera_rig: Node3D = $CameraRig
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var pivot: Node3D = $BodyPivot
-@onready var raycast: RayCast3D = $BottomPivot/RayCast
+@onready var spring_raycast: RayCast3D = $BottomPivot/SpringRayCast
+@onready var ground_raycast: RayCast3D = $BottomPivot/GroundRayCast
 
 var jump_input: float = 0.0
 var can_move: bool = true
@@ -29,8 +30,12 @@ var target_velocity: Vector3 = Vector3.ZERO
 
 func _ready() -> void:
 	# Setup RayCast
-	raycast.target_position = Vector3(0.0, (-2 * ride_height), 0.0)
-	raycast.add_exception(self)
+	spring_raycast.target_position = Vector3(0.0, -(2 * ride_height), 0.0)
+	spring_raycast.add_exception(self)
+	ground_raycast.target_position = Vector3(0.0, -(ride_height + 0.1), 0.0)
+	ground_raycast.add_exception(self)
+	
+	# Start animation
 	animation_player.play("idle")
 
 
@@ -58,28 +63,23 @@ func update_ground_velocity(_delta: float) -> void:
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
 
-
+var time_since_jump_pressed: float = 0.0
 # Update the character's target AIR velocity
-# TODO: variable jump heights based on how long the button was pressed
+# TODO: variable jump heights based on how long the button is held
 func update_vertical_velocity(delta: float) -> void:
-	var ray_origin: Vector3 = raycast.global_transform.origin
-	var ray_hit: Vector3 = raycast.get_collision_point()
+	var grounded: bool = is_grounded()
+	if is_on_ceiling():
+		target_velocity.y = 0.0
 	
-	# If not 0, cur_height is the distance away from the desired ride height
-	var cur_height: float = ray_origin.y - ray_hit.y - ride_height
+	if Input.is_action_just_pressed("jump") and grounded:
+		time_since_jump_pressed = 0.05
+		target_velocity.y = 25.0
+	elif time_since_jump_pressed <= 0.0:
+		apply_spring_force()
+		target_velocity.y -= (fall_accel * delta) # Gravity
 	
-	# Get the dot product of the character's movement velocity and 
-	# the direction of the raycast. 
-	var relative_velocity: float = velocity.dot(-raycast.transform.basis.y)
-	
-	# Calculate the spring force needed to maintain ride height
-	var spring_force: float = (cur_height * ride_spring_strength) - \
-			(relative_velocity * ride_spring_damp)
-	
-	# Update air velocity
-	if (raycast.is_colliding()):
-		target_velocity.y -= spring_force     # Spring
-	target_velocity.y -= (fall_accel * delta) # Gravity
+	if time_since_jump_pressed > 0.0:
+		time_since_jump_pressed -= delta
 
 
 func get_movement_input_vector() -> Vector3:
@@ -96,6 +96,30 @@ func get_movement_input_vector() -> Vector3:
 		input = input.normalized()
 	
 	return input
+
+
+func apply_spring_force() -> void:
+	var ray_origin: Vector3 = spring_raycast.global_transform.origin
+	var ray_hit: Vector3 = spring_raycast.get_collision_point()
+	
+	# If not 0, cur_height is the distance away from the desired ride height
+	var cur_height: float = ray_origin.y - ray_hit.y - ride_height
+	
+	# Get the dot product of the character's movement velocity and 
+	# the direction of the raycast. 
+	var relative_velocity: float = velocity.dot(-spring_raycast.transform.basis.y)
+	
+	# Calculate the spring force needed to maintain ride height
+	var spring_force: float = (cur_height * ride_spring_strength) - \
+			(relative_velocity * ride_spring_damp)
+	
+	# Update air velocity
+	if (spring_raycast.is_colliding()):
+		target_velocity.y -= spring_force  # Spring
+
+
+func is_grounded() -> bool:
+	return ground_raycast.is_colliding()
 
 
 func _on_gui_can_move(val: bool) -> void:
